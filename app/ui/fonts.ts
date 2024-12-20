91f4017e141a46701e5b619dd393618df37bd5f5
inspiringforever <wenjieaha@outlook.com>
2024-12-20 16:05:38 +0800

    1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Inter,Lusitana} from 'next/font/google'
+
+export const lusitana = Inter({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})// 导入 Inter 字体，并设置字体子集为 latin
+export const inter = Inter({
+  subsets: ['latin'],
+})// 导入 Inter 字体，并设置字体子集为 latin
