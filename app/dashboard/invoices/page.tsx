5edb00623afb824d7036bd58c946b8f8bdc95076
inspiringforever <wenjieaha@outlook.com>
2024-12-23 19:26:55 +0800

    4

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
+'use strict'
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
@@ -7,6 +8,12 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
+import { Metadata } from 'next';
+import { log } from 'console';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page(
   props: {
     searchParams?: 
@@ -20,7 +27,6 @@ export default async function Page(
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
-
 
   return (
     <div className="w-full">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,21 +8,21 @@ import {
   useRouter,
 } from 'next/navigation';
 
+
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  
   const pathname = usePathname();
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback(
     (text:string)=>{
 
-      text = text.trim();
-      console.log(text);
       const param = new URLSearchParams(searchParams);
       param.set('page', '1')
       if (text) {
-        param.set('qery', text);
+        param.set('query', text);
       }else{
-        param.delete('qery');
+        param.delete('query');
       }
       replace(`${pathname}?${param.toString()}`);
   },300);
@@ -35,6 +35,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e)=>handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
